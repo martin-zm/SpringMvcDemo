6aa4258e0885eba0ded688bc460a0c3f899e5f30
zm <15071239679@163.com>
2017-08-17 16:15:09 +0800

    first commit

---
+++ springmvc-2/src/com/atguigu/springmvc/test/SpringMVCTestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.atguigu.springmvc.test;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+//更全局的异常
+@ControllerAdvice
+public class SpringMVCTestExceptionHandler {
+
+	@ExceptionHandler({ArithmeticException.class})
+	public ModelAndView handleArithmeticException(Exception ex){
+		System.out.println("----> 出异常了: " + ex);
+		ModelAndView mv = new ModelAndView("error");
+		mv.addObject("exception", ex);
+		return mv;
+	}	
+}
---
+++ springmvc-3/src/com/atguigu/springmvc/UserService.java
@@ -0,0 +1,15 @@
+package com.atguigu.springmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+//	@Autowired
+//	private HelloWorld helloWorld;
+	
+	public UserService() {
+		System.out.println("UserService Constructor...");
+	}	
+}
